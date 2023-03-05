31cd9a411b20731087d3e2dc9dc083c082ec9a09
gmoroz <12345hasek@gmail.com>
2023-03-05 08:31:26 +0300

    feat: add form_check task

--- bot/celery_config.py
+++ bot/celery_config.py
@@ -1,6 +1,8 @@
 from celery import Celery
 from bot import settings
 from bot.utils import form_write
+import asyncio
+from aiohttp import ClientSession
 
 app = Celery(
     "tasks", broker=settings.CELERY_BROKER_URL, backend=settings.CELERY_BROKER_URL
@@ -10,3 +12,23 @@ app = Celery(
 @app.task
 def form_write_task(data: dict[str, str | int]) -> str:
     return form_write(data)
+
+
+async def check_form_availability():
+    while True:
+        async with ClientSession() as session:
+            async with session.get(settings.FORM_URL) as response:
+                if response.status == 200:
+                    print("Форма доступна!")
+                else:
+                    print(f"С формой что-то не так. Код ответа: {response.status}")
+        await asyncio.sleep(600)
+
+
+@app.task
+def check_form_availability_task():
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(check_form_availability())
+
+
+check_form_availability_task.delay()
