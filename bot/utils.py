9f8d94dd48896ca959c930540830eb4eeaf0f972
gmoroz <12345hasek@gmail.com>
2023-03-05 07:37:00 +0300

    fix: import

--- bot/utils.py
+++ bot/utils.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import settings
+from bot import settings
 from selenium.webdriver.common.action_chains import ActionChains
 
 
