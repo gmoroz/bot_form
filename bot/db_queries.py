cfc4320ee2709d39804abbfeb574d26bb31ec208
gmoroz <12345hasek@gmail.com>
2023-03-05 07:36:01 +0300

    feat: add save user to db

--- bot/db_queries.py
+++ bot/db_queries.py
@@ -1,2 +1,8 @@
-async def save_data_to_db(data: dict[str, str]) -> None:
-    pass
+from bot.database import engine
+from bot.models import users
+
+
+async def save_data_to_db(data: dict[str, str]) -> int:
+    async with engine.begin() as conn:
+        result = await conn.execute(users.insert(), [data])
+        return result.inserted_primary_key[0]
